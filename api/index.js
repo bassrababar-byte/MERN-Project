818f33f8bb48e8ba359c8b0d348f19bbbe1b17d9
Basra Babar <bassrababar@gmail.com>
2025-10-16 19:20:39 +0500

    test user commit

--- api/index.js
+++ api/index.js
@@ -44,14 +44,14 @@ app.use('/api/listing', listingRouter);
 
 
 
-// app.get('/api/test', async (req, res) => {
-//   try {
-//     const data = await MongooseModel.find();
-//     res.status(200).json(data);
-//   }catch (error) {
-//     res.status(500).json({ message: 'Error fetching data', error });
-//   }
-// });
+app.get('/api/test', async (req, res) => {
+  try {
+    const data = await MongooseModel.find();
+    res.status(200).json(data);
+  }catch (error) {
+    res.status(500).json({ message: 'Error fetching data', error });
+  }
+});
 
 
 
