cb80941185fbdd884f3174a694f2f7cb9608fe93
Basra Babar <bassrababar@gmail.com>
2025-10-16 15:04:31 +0500

    first commit

---
+++ MERN-Stack/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-stack-ad653.firebaseapp.com",
+  projectId: "mern-stack-ad653",
+  storageBucket: 'mern-estate.appspot.com',
+  messagingSenderId: "84526537135",
+  appId: "1:84526537135:web:0c363888d3cc310b7cb75a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ api/index.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js';
+
+
+
+
+let isConnected = false;
+//new connection for vercel
+async function connectToMongoDB() {
+  try{
+    await mongoose.connect(process.env.MONGO);
+    isConnected = true;
+    console.log("connected to MongoDB");
+  }catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+
+//routes
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+
+//middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+
+
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
+
+//add new middleware for deploy on vercel
+app.use((req, res, next) => {
+  if (!isConnected){
+    connectToMongoDB();
+  }
+  next();
+})
+
+export default app;
