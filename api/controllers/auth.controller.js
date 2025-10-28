df96009e97df8dea332138e5ce087051c6c24391
Basra Babar <bassrababar@gmail.com>
2025-10-28 17:19:48 +0500

    auth commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -29,7 +29,12 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie('access_token', token, { httpOnly: true })
+      .cookie('access_token', token, {
+          httpOnly: true,
+          secure: true,       // required if SameSite=None
+          sameSite: 'none',   // allow cross-site
+          path: '/',
+        })
       .status(200)
       .json(rest);
   } catch (error) {
